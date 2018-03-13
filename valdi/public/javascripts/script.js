fc41eef72787e894c200ac650340fc8e5509268b
Marika Moreau <89marika.moreau@gmail.com>
2018-03-13 16:03:01 +0100

    försöker fixa menyn
    eftersom sidan laddar om layout, tror jag, när man klickar på en länk i huvudmenyn så återvänder klassen active alltid till hem. osäker på hur man bryter den funktionen eller skriver över det.

--- valdi/public/javascripts/script.js
+++ valdi/public/javascripts/script.js
@@ -1,6 +1,3 @@
-$("li").click(function() {
-      // remove classes from all
-      $("li").removeClass("active");
-      // add class to the one we clicked
-      $(this).addClass("active");
-   });
+$('.ui.dropdown')
+  .dropdown()
+;
