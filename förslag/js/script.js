870ee1310f8b5cbbe3b98998484564fbaa489776
jessicamartensson <32508948+jessicamartensson@users.noreply.github.com>
2018-03-16 14:47:14 +0100

    menyknapp mobil

--- förslag/js/script.js
+++ förslag/js/script.js
@@ -1,9 +1,23 @@
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 window.onload = start;
+
+var showMenu = document.querySelector("#menu-btn");
+
+showMenu.addEventListener("click", function() {
+  console.log("inside the function")
+  var nav = document.querySelector("#nav");
+  if (window.innerWidth <= 480) {
+    if (nav.style.display === "none") {
+      nav.style.display = "block";
+    } else {
+      nav.style.display = "none";
+    }
+  }
+});
 
 function start(){
     console.log("Hej");
@@ -18,4 +32,3 @@ function mobilNav(){
         menu.className = "none";
     }
 }
-
